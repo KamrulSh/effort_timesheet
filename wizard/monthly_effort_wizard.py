7514081ff77eb8af41f61455c9512c2818c6f299
Kamrul Islam <shahin.cuetcse@gmail.com>
2022-02-18 12:42:24 +0600

    generate month and year wise effort table

--- wizard/monthly_effort_wizard.py
+++ wizard/monthly_effort_wizard.py
@@ -1,5 +1,4 @@
 from odoo import models, fields, api, _
-from datetime import datetime, timedelta
 
 
 class MonthlyEffort(models.TransientModel):
@@ -8,11 +7,11 @@ class MonthlyEffort(models.TransientModel):
     def _get_employee_id(self):
         return self.env.user.employee_id
 
-    project_id = fields.Many2one('project.project', string="Project name")
+    project_id = fields.Many2one('project.project', string="Project name", required=True)
     employee_id = fields.Many2one('hr.employee', string="Employee", default=_get_employee_id, required=True)
-    effort_month = fields.Date("Month", default=fields.Datetime.now())
+    effort_month = fields.Date("Month", default=fields.Datetime.now(), required=True)
 
-    monthly_report = fields.Html()
+    monthly_report = fields.Html("Monthly Effort")
 
     def month_effort_query(self):
         query = f"""
@@ -23,26 +22,28 @@ class MonthlyEffort(models.TransientModel):
                         effort_timesheet et
                     LEFT JOIN hr_employee hr ON hr.id = et.employee_id
         			LEFT JOIN project_project pp ON pp.id = et.project_id
+        			WHERE TO_CHAR(et.start_time, 'MM')::int = {self.effort_month.strftime('%m')}
+        			AND TO_CHAR(et.start_time, 'YYYY')::int = {self.effort_month.strftime('%Y')}
                 """
-        where = f"""
-                    WHERE et.employee_id = {self.employee_id.id}
+        query_employee = f"""
+                    AND et.employee_id = {self.employee_id.id}
                 """
-        where_and = f"""
+        query_project = f"""
                         AND et.project_id = {self.project_id.id}
                     """
         orderby = f"""
                     ORDER BY start_time
                 """
         if self.project_id and self.employee_id:
-            self._cr.execute(query + where + where_and + orderby)
+            self._cr.execute(query + query_employee + query_project + orderby)
         elif self.employee_id:
-            self._cr.execute(query + where + orderby)
+            self._cr.execute(query + query_employee + orderby)
         else:
             self._cr.execute(query + orderby)
         effort_data = self._cr.dictfetchall()
         return effort_data
 
-    @api.onchange("project_id", "employee_id")
+    @api.onchange("project_id", "employee_id", "effort_month")
     def action_preview_report(self):
         table = """
                     <table border="1" class="o_list_view table table-condensed table-striped o_list_view_ungrouped">
@@ -74,9 +75,7 @@ class MonthlyEffort(models.TransientModel):
         count_days = 0
         for i in range(0, len(effort_data)):
             count_days += 1
-            column_value = [
-                str(effort_data[i]['start_time'].day) + " " + str(effort_data[i]['start_time'].strftime('%B')),
-                effort_data[i]['duration']]
+            column_value = [str(effort_data[i]['start_time'].strftime('%a, %d %B, %Y')), effort_data[i]['duration']]
             total_duration += effort_data[i]['duration']
             body += tr.format("".join(map(td.format, column_value)))
         total_duration_field = [f"Total days: {count_days}", f"Total effort (hours): {total_duration}"]
